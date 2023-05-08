9841040ec6c29b5337201817d60b26b86c5b15c3
Ahmed0548 <ahmedalkasape@gmail.com>
2023-05-08 10:25:29 -0700

    Update removeDuplicates

--- EASY/RemoveDuplicates.java
+++ EASY/RemoveDuplicates.java
@@ -1,13 +1,23 @@
+/*
+ * The code implements the approach of two pointers
+ * (i) keeps track of the index where a unique element should be placedin array
+ * other pointer (j) loops througth the array to check for duplicates
+ * if the element at index (j) not equal to element at index (i) it means
+ * that it is a new unique element and it should be placed at index i + 1
+ */
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int index = 1;
-       for (int i = 0; i < nums.length - 1; i++){
-           if (nums[i] != nums[i + 1]){
-               nums[index] = nums[i +1];
-               index++;
+       if(nums.length == 0) {
+	       return 0;
+       }
+       int i = 0;
+       for (int j = 1; j < nums.length; j++){
+           if (nums[j] != nums[i]){
+               i++;
+	       nums[i] = nums[j];
            }
 
        }
-       return index;
+       return i + 1;
     }
 }
