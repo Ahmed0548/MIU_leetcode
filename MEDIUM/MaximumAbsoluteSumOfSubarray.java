d1a1c79214b07186141c7e5fa77a96a47497adde
Ahmed0548 <ahmedalkasape@gmail.com>
2023-05-29 05:22:07 -0700

    absolute sum

---
+++ MEDIUM/MaximumAbsoluteSumOfSubarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int max = 0;
+        int sum = 0;
+        int min = 0;
+        for(int num :nums) {
+            sum += num;
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
+        }
+        return max - min;
+    }
+}
