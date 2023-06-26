c879aab731fd6b7dc0a5b3944e4627627c5196f1
Ahmed0548 <ahmedalkasape@gmail.com>
2023-06-26 08:51:33 -0700

    Inertial

--- EASY/inertial.java
+++ EASY/inertial.java
@@ -1,23 +1,25 @@
 class Solution {
     public static boolean isInertial(int[] arr) {
-        boolean hasOdd = false;
-        boolean hasMaxEven = false;
         int max = Integer.MIN_VALUE;
+        boolean hasOdd = false;
+        boolean hasEven = false;
+        boolean result = true;
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i] % 2 != 0) {
+            if(arr[i] % 2 == 1) {
                 hasOdd = true;
-                if(arr[i] >= max) {
-                    max = arr[i];
-                }else{
-                    return false;
+                if(arr[i] <= max && arr[i] != arr[arr.length -1]) {
+                    result = false;
                 }
             }else{
-                if(arr[i] >= max) {
-                    hasMaxEven = true;
+                hasEven = true;
+                if(arr[i] > max) {
                     max = arr[i];
                 }
             }
+            if(!hasOdd ||(!hasEven && arr[arr.length - 1] % 2 == 0)) {
+                result = false;
+            }
         }
-        return hasOdd && hasMaxEven;
+        return result;
     }
 }
